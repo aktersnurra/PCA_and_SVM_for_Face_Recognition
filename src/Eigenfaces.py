75940a7f90c97ceed059b3313073d8765488e141
aktersnurra <grydholm@kth.se>
2016-11-29 14:59:35 +0000

    Updates in Eigenfaces

--- src/Eigenfaces.py
+++ src/Eigenfaces.py
@@ -2,6 +2,7 @@ from __future__ import division
 import numpy as np
 import matplotlib.pyplot as plt
 from numpy import linalg as LA
+import sys
 
 from ReadFaces import ReadFaces
 from EigenObject import EigenObject
@@ -45,6 +46,7 @@ class EigenFaces(object):
         self.AT = np.zeros((self.N, self.D)).astype(float)
         self.path_to_lib = '/Users/Gustaf/Dropbox KTH/Dropbox/KTH/Imperial College London/kurser/' \
                            'autumn/pattern recognition/cw/PCA and SVM for face recognition/lib/'
+        self.best_eigfaces = []
 
     def __call__(self):
         '''
@@ -53,36 +55,40 @@ class EigenFaces(object):
         self.main()
 
     def main(self):
-        var = str(raw_input('Please select a or b:'))
+        var_1 = str(raw_input('Please select a or b:\n'))
         self.compute_avg_face_vector()
         self.compute_A()
 
-        if var == 'a':
+        if var_1 == 'a':
             self.compute_covariance_matrix()
-            self.compute_principal_components(var)
+            self.compute_principal_components(var_1)
 
-        elif var == 'b':
+        elif var_1 == 'b':
             self.compute_data_matrix_b()
-            self.compute_principal_components(var)
+            self.compute_principal_components(var_1)
+
+        else:
+            sys.exit(0)
 
         self.faces_onto_eigenfaces()
 
-        var = int(raw_input('Which face do you want to reconstruct?'))
-        self.face = var
-        self.reconstruct_face()
-        self.display_rec_face()
+        var_2 = int(raw_input('Which face do you want to reconstruct?\n'))
+        self.face = var_2
+        #self.reconstruct_face()
+        self.reconstruct_test_face()
+        self.face = var_2
 
-        var = str(raw_input('Display plots? (y/n):'))
-        if var == 'y':
-            self.display_eigenvalues()
-            self.save_faces()
+        var_3 = str(raw_input('Display faces? (y/n)\n'))
+        if var_3 == 'y':
+            self.save_faces(var_1)
+
+        var_4 = str(raw_input('Compute the error/display plots? (y/n)\n'))
+        if var_4 == 'y':
             self.compute_zero_eigenvalues()
-
-        var = str(raw_input('Compute the error?'))
-        if var == 'y':
             self.distortion_measure()
-            self.compute_error()
-            self.display_error()
+            self.compute_reconstruction_error()
+            self.display_error(var_1)
+            self.display_eigenvalues(var_1)
 
     def compute_avg_face_vector(self):
         '''
@@ -170,13 +176,18 @@ class EigenFaces(object):
         phi = np.zeros((self.D, 1)).astype(float)
         for k in xrange(0, self.N):
             phi[:, 0] = self.A[:, k]
-            weight_array = np.zeros(self.M)
-            for i in xrange(0, self.M):
-                u = self.eig_arr[i].get_eigenvector()
-                phi_transpose = np.transpose(phi)
-                weight = phi_transpose.dot(u)
-                weight_array[i] = weight
-            self.weight_matrix.append(weight_array)
+            self.eigenspace(phi)
+
+    def eigenspace(self, phi):
+        weight_array = np.zeros(self.M)
+
+        for i in xrange(0, self.M):
+            u = self.eig_arr[i].get_eigenvector()
+            phi_transpose = np.transpose(phi)
+            weight = phi_transpose.dot(u)
+            weight_array[i] = weight
+
+        self.weight_matrix.append(weight_array)
 
     def reconstruct_face(self):
         sum_eig_vectors = np.zeros((self.D, 1)).astype(float)
@@ -187,6 +198,13 @@ class EigenFaces(object):
             sum_eig_vectors += weight * u
         self.x_tilde[:, 0] = self.x_bar[:, 0] + sum_eig_vectors[:, 0]
 
+    def reconstruct_test_face(self):
+        face_vector = self.test_data[self.face].get_face_vector()
+        phi = face_vector[:, 0] - self.x_bar[:, 0]
+        self.eigenspace(phi)
+        self.face = len(self.weight_matrix) - 1
+        self.reconstruct_face()
+
     def compute_zero_eigenvalues(self):
         threshold = 1e-10
         counter = 0
@@ -195,14 +213,19 @@ class EigenFaces(object):
             if np.absolute(eigenvalue) < threshold:
                 eig_zero_arr.append(eigenvalue)
                 counter += 1
-        print counter, eig_zero_arr
+        print 'Number of eigenvalues that are zero:', counter, eig_zero_arr
 
-    def compute_error(self):
-        original_face = np.copy(self.training_data[self.face].get_face_vector())
+    def compute_reconstruction_error(self):
+        original_face = np.copy(self.test_data[self.face].get_face_vector())
+        #original_face = np.copy(self.training_data[self.face].get_face_vector())
         arr_of_rec_face = []
+        self.display_face(original_face, 'original_face')
 
         for subspace in self.arr_of_subspaces:
             self.M = subspace
+
+            self.face = len(self.weight_matrix) - 1
+
             self.reconstruct_face()
             arr_of_rec_face.append(np.copy(self.x_tilde))
 
@@ -215,30 +238,36 @@ class EigenFaces(object):
         error = 0
         start = self.M + 1
         stop = len(self.eig_values)
+
         for i in xrange(start, stop):
             error += self.eig_values[i]
+
+        text_file = open('distortion_measure.txt', 'w')
+        text_file.write('distortion measure' % error)
+        text_file.close()
         print 'distortion measure', error
 
-    def save_faces(self):
+    def save_faces(self, var):
         '''
         Save each face to /lib as .pdf.
-        :return:
+        :return: N/A
         '''
-        self.display_face(self.x_bar, '/avg_face_vector.pdf')
+        self.display_face(self.x_bar, '/avg_face_vector_' + var + '.png')
 
-        for i in range(0, 5):
-            title = '/eig_face_vector' + str(i) + '.pdf'
-            self.display_face(self.eig_arr[i].get_eigenvector(), title)
+        for i in xrange(0, 25):
+            self.best_eigfaces.append(self.eig_arr[i].get_eigenvector().reshape((46, 56)).transpose())
 
-        self.display_face(self.x_tilde, 'reconstructed_face' + str(self.face) + '.pdf')
+        self.display_eigenface(var)
 
-    def display_eigenvalues(self):
+        self.display_face(self.x_tilde, 'reconstructed_face_' + str(self.face) + '_' + var + '.png')
+
+    def display_eigenvalues(self, var):
         '''
         Plots eigenvalues vs dimension of the images.
         '''
-        title = self.path_to_lib + '/eigvalues_vs_dim_500zoom.pdf'
+        title = self.path_to_lib + '/eigvalues_vs_dim_250zoom_' + var + '.pdf'
         plt.ticklabel_format(style='sci', axis='y', scilimits=(1, 3))
-        plt.plot(np.array(np.arange(self.D)), self.eig_values[:])
+        plt.plot(np.array(np.arange(len(self.eig_values))), self.eig_values[:])
         plt.ylim([self.eig_values[-1], self.eig_values[0]])
         plt.xlim([0, 250])
         plt.xlabel('Dimension')
@@ -250,21 +279,29 @@ class EigenFaces(object):
         img = face_vector.reshape((46, 56)).transpose()
         plt.figure(figsize=(0.48, 0.58))
         plt.imshow(img, cmap='Greys_r')
+        plt.axis('tight')
         plt.axis('off')
-        plt.savefig(self.path_to_lib + title)
+        plt.savefig(self.path_to_lib + title, transparent='True')
         plt.show()
 
-    def display_rec_face(self):
-        img = self.x_tilde.reshape((46, 56)).transpose()
-        plt.figure(figsize=(0.48, 0.58))
-        plt.imshow(img, cmap='Greys_r')
-        plt.axis('off')
+    def display_eigenface(self, var):
+        f, axs = plt.subplots(5, 5)
+        img = 0
+        title = self.path_to_lib + 'best_eigenfaces_' + var + '.png'
+
+        for i in xrange(0, 5):
+            for j in xrange(0, 5):
+                axs[i, j].imshow(self.best_eigfaces[img], cmap='Greys_r')
+                axs[i, j].axis('off')
+                img += 1
+
+        plt.savefig(title, transparent='True')
         plt.show()
 
-    def display_error(self):
-        title = self.path_to_lib + '/rec_error.pdf'
-        plt.plot(self.arr_of_subspaces[:], self.error_arr[:])
-        plt.ylim([self.error_arr[-1], self.error_arr[0] + 16])
+    def display_error(self, var):
+        title = self.path_to_lib + '/rec_error_' + var + '.pdf'
+        plt.stem(self.arr_of_subspaces[:], self.error_arr[:])
+        plt.ylim([0, self.error_arr[0] + 10])
         plt.xlim([self.arr_of_subspaces[0] - 10, self.arr_of_subspaces[-1] + 10])
         plt.xlabel('Subspace dimension')
         plt.ylabel('Error')
